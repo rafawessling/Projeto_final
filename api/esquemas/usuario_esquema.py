50deb62f4477c298ea0855aa7cdf4a58298046ae
Bruna Faleiros <faleiros.b@gmail.com>
2022-10-04 19:51:28 -0300

    correções no esquema do usuário

--- api/esquemas/usuario_esquema.py
+++ api/esquemas/usuario_esquema.py
@@ -1,9 +1,7 @@
-from typing import List
-from pydantic import BaseModel, Field
-
+from pydantic import BaseModel, Field, EmailStr
 class User(BaseModel):
-    name: str = Field(...)
-    email: str = Field(..., unique=True)
-    password: str = Field(...)
-    adress: str = Field(...)
+    name: str = Field(..., min_length=3, max_length=100, description="Nome completo")
+    email: EmailStr = Field(..., unique=True, description="E-mail")
+    password: str = Field(..., min_length=8, max_length=20, description="Senha")
+    adress: str = Field(..., min_length=5, max_length=50, description="Endereço")
     id: int 
