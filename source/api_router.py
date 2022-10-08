fc7811d0f3ee8ceccd8a75c45ec2c733d570be8b
giugabriella <giuh.gabriella@gmail.com>
2022-10-08 17:30:31 -0300

    Atualização dos arquivos main, router e database

--- source/api_router.py
+++ source/api_router.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
-from routes import address_route
+from routes import address_route, user_route
 
 router = APIRouter()
 
 router.include_router(address_route.router)
+router.include_router(user_route.router)
--- source/main.py
+++ source/main.py
@@ -1,14 +1,16 @@
-#import email
 from fastapi import FastAPI
-from typing import List
 from pydantic import BaseModel
-from api_router import router 
+from api_router import router
+from server.database import DataBase
 import uvicorn
-
+from dotenv import load_dotenv
+from pathlib import Path
 
 app = FastAPI()
 
 app.include_router(router)
+load_dotenv(Path('main.py').resolve().parents[1].joinpath('.env'))
+db = DataBase()
 
 if __name__ == '__main__':
     uvicorn.run("main:app", host='127.0.0.1', port=8005, log_level="info", reload=True)
