dd31e61cc3a370eb0f9c8ccae843d8ddfb825614
Bruna Faleiros <faleiros.b@gmail.com>
2022-10-09 01:05:42 -0300

    User validation

---
+++ source/validations/user_validation.py
@@ -0,0 +1,34 @@
+import re
+from fastapi.exceptions import HTTPException
+from Projeto_final.source.schemas.user_schema import User
+from source.schemas.user_schema import UserSchema, UpdateUser
+import logging
+from source.server.database import db
+
+logger = logging.getLogger(__name__)
+
+async def create_user(email: User):
+    if User.email == email:
+        raise TypeError("E-mail já cadastrado.")
+    else: 
+        user = db.users_collection.insert_one(user.dict())
+        return{
+            "status": "Usuário cadastrado com suceso!",
+            "usuario": user.dict()
+        }
+
+async def validate_email(email: User):
+    regex = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
+
+    if len(email) < 3:
+        raise TypeError("E-mail inválido!")
+    if (re.search(regex, email)):
+        return None
+    else:
+        return ("E-mail inválido!")
+
+async def get_user_by_email(email: User):
+    if User.email in db.users_collection:
+        return User
+    else:
+        return("Usuário não encontrado!")
