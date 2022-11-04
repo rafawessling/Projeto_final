ff1a7472a427e75721167144a1f91627112e1055
rafawessling <rafaoening@gmail.com>
2022-11-04 17:11:03 +0100

    Adicionado class Config em cart_schema

--- source/schemas/cart_schema.py
+++ source/schemas/cart_schema.py
@@ -1,7 +1,14 @@
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 class CartSchema(BaseModel):
-    user_id: object 
-    total_price: float
-    
+    user_id: object = Field(..., description="Id do usuário")
+    total_price: float = Field(..., description="Preço total")
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "user_id": "634dd501b6101538bd964b0d",
+                "total_price": 0
+            }
+        }
