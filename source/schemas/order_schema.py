8c342681cf6edc737d1ecdc842aade270be9cd44
rafawessling <rafaoening@gmail.com>
2022-11-04 17:36:43 +0100

    Adicionado class Config em order_schema

--- source/schemas/order_schema.py
+++ source/schemas/order_schema.py
@@ -5,9 +5,21 @@ from schemas.order_items_schema import OrderItemsSchema
 
 
 class OrderSchema(BaseModel):
-    userId: object
+    userId: object = Field(..., description="Id do usuário")
     order_item: List[OrderItemsSchema] = []
-    price: float = Field(...)
-    paid: bool = Field(default=False)
-    created: datetime.datetime = Field(default=datetime.datetime.now())
-    addressId: object 
+    price: float = Field(..., description="Preço total do pedido")
+    paid: bool = Field(default=False, description="Se o pagamento foi efetuado")
+    created: datetime.datetime = Field(default=datetime.datetime.now(), description="Data de criação do pedido")
+    addressId: object = Field(..., description="Id do endereço")
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "userId": "634dd501b6101538bd964b0d",
+                "order_item": [],
+                "price": 0,
+                "paid": False,
+                "created": datetime.datetime.now(),
+                "addressId": "634dd520b6101538bd964b10"
+            }
+        }
