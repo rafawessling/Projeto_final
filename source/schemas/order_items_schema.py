3a6e71f082afb4d0de078e6eaf200ff7204c2d2a
rafawessling <rafaoening@gmail.com>
2022-11-04 17:23:55 +0100

    Adicionado class Config em order_items_schema

--- source/schemas/order_items_schema.py
+++ source/schemas/order_items_schema.py
@@ -1,7 +1,16 @@
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 class OrderItemsSchema(BaseModel):
-    product_code: str
-    quantity: int = 0
-    cartId: object 
+    product_code: str = Field(..., description="Código do produto")
+    quantity: int = Field(..., ge=1, description="Quantidade de itens")
+    cartId: object = Field(..., description="Id do cart")
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "product_code": "123456",
+                "quantity": "3",
+                "cartId": "63653c06a83bfb51873d1669"
+            }
+        }
